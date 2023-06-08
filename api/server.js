54ec21ecf09b2b5439b766f5d3b0ec55b2d4b7a5
RobJ1925 <robj1925@gmail.com>
2023-06-08 16:58:04 -0400

    connected database

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/task-tracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch(console.error);
+
+  app.listen(3001, () => console.log("Server stated on poort 3001"));
+
