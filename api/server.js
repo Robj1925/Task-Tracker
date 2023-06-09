9ecb245796961e795a0bb6260bd5cb2ea7e651f9
RobJ1925 <robj1925@gmail.com>
2023-06-09 15:07:38 -0400

    receive json (empty)

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -15,5 +15,22 @@ mongoose
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
+  const Todo = require('./models/Todo');
+
+  app.get('/todos', async (req,res) => {
+    const todos = await Todo.find();
+
+    res.json(todos);
+
+  });
+
+  app.post('/todo/new', (req, res) => {
+      const todo = new Todo({
+        text: req.body.text
+      });
+
+      todo.save();
+      res.json(todo);
+  });
   app.listen(3001, () => console.log("Server stated on poort 3001"));
 
