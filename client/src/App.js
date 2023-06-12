89938579cdbd70e2454169196df2ee8260eb7c0d
RobJ1925 <robj1925@gmail.com>
2023-06-12 18:50:35 -0400

    started working on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a //testing wkcwcjwevnjwev did it work efvevf
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Welcome Rob J</h1>
+        <h4>Your Tasks</h4>
+        <div className="todos"> 
+          <div className="todo">
+            <div className="checkbox">
+              <div className="text">
+                Get the Bread
+                
+                <div className="delete-todo">x</div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
