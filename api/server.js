85bca2f75031df760361055933411776bc231ad8
RobJ1925 <robj1925@gmail.com>
2023-06-14 11:12:53 -0400

    finished styling, delete, add, implemention

--- api/server.js
+++ api/server.js
@@ -39,7 +39,7 @@ mongoose
     res.json(result);
   })
 
-  app.put('/todo/complete/:id', async (req, res) => {
+  app.get('/todo/complete/:id', async (req, res) => {
     const todo = await Todo.findById(req.params.id); 
 
     todo.complete = !todo.complete;
--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,42 @@ function App() {
       .then((data) => setTodos(data))
       .catch((err) => console.error("Error: ", err));
   };
+
+  const completeTodo = async (id) => {
+    const data = await fetch(API_BASE + "/todo/complete/" + id).then((res) =>
+      res.json()
+    );
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo._id === data._id) {
+          todo.complete = data.complete;
+        }
+        return todo;
+      })
+    );
+  };
+
+  const deleteTodo = async (id) => {
+    const data = await fetch(API_BASE + "/todo/delete/" + id, {
+      method: "DELETE",
+    }).then((res) => res.json());
+
+    setTodos((todos) => todos.filter((todo) => todo._id !== data._id));
+  };
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + "/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then(res => res.json());
+    setTodos([...todos, data]);
+    setPopupActive(false);
+    setNewTodo("");
+  };
   return (
     <div class="App">
       <h1>Welcome Rob J</h1>
@@ -28,13 +64,40 @@ function App() {
           <div
             class={"todo " + (todo.complete ? "is-complete" : "")}
             key={todo._id}
+            onClick={() => completeTodo(todo._id)}
           >
             <div class="checkbox"></div>
             <div class="text">{todo.text}</div>
-            <div class="delete-todo">x</div>
+            <div class="delete-todo" onClick={() => deleteTodo(todo._id)}>
+              x
+            </div>
           </div>
         ))}
       </div>
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            x
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
