d8f383b7a4318ad9a0ed131bd33f29b92378b87f
RobJ1925 <robj1925@gmail.com>
2023-06-14 00:56:29 -0400

    UI almost finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,40 @@
+import { useState, useEffect } from "react";
+
+const API_BASE = "http://localhost:3001";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    GetTodos();
+
+    console.log(todos);
+  }, []);
+
+  const GetTodos = () => {
+    fetch(API_BASE + "/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.error("Error: ", err));
+  };
   return (
-    <div className="App">
-        <h1>Welcome Rob J</h1>
-        <h4>Your Tasks</h4>
-        <div className="todos"> 
-          <div className="todo">
-            <div className="checkbox">
-              <div className="text">
-                Get the Bread
-                
-                <div className="delete-todo">x</div>
-              </div>
-            </div>
+    <div class="App">
+      <h1>Welcome Rob J</h1>
+      <h4>Your Tasks</h4>
+      <div class="todos">
+        {todos.map((todo) => (
+          <div
+            class={"todo " + (todo.complete ? "is-complete" : "")}
+            key={todo._id}
+          >
+            <div class="checkbox"></div>
+            <div class="text">{todo.text}</div>
+            <div class="delete-todo">x</div>
           </div>
-        </div>
+        ))}
+      </div>
     </div>
   );
 }
